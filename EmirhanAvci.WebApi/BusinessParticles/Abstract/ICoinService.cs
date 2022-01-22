3525bb12e6f42e6d298cb775b5d32321163cd0c1
Emirhan <emir97han@gmail.com>
2022-01-22 11:16:35 +0300

    first commit

---
+++ EmirhanAvci.WebApi/BusinessParticles/Abstract/ICoinService.cs
@@ -0,0 +1,19 @@
+﻿using EmirhanAvci.WebApi.Models.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmirhanAvci.WebApi.BusinessParticles.Abstract
+{
+    #region NamingDescription
+    /*
+        Main folder name is BusinessParticles.Normally we can name it  
+     */
+    #endregion
+
+    public interface ICoinService
+    {
+        List<Coin> GetAll();
+    }
+}
---
+++ EmirhanAvci.WebApi/BusinessParticles/Concrete/CoinManager.cs
@@ -0,0 +1,24 @@
+﻿using EmirhanAvci.WebApi.BusinessParticles.Abstract;
+using EmirhanAvci.WebApi.DataParticles.Abstract;
+using EmirhanAvci.WebApi.FixedDataOperations.DataListOperations;
+using EmirhanAvci.WebApi.Models.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmirhanAvci.WebApi.BusinessParticles.Concrete
+{
+    public class CoinManager : ICoinService
+    {
+        ICoinParticleGeneratorService _coinParticleGeneratorService;
+        public CoinManager(ICoinParticleGeneratorService coinParticleGeneratorService)
+        {
+            _coinParticleGeneratorService = coinParticleGeneratorService;
+        }
+        public List<Coin> GetAll()
+        {
+            return _coinParticleGeneratorService.GetAll();
+        }
+    }
+}
